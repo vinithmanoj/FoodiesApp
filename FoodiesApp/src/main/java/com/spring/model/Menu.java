936f7588c648919532cdc46031ea9da9cc586140
vinithmanoj <33922782+vinithmanoj@users.noreply.github.com>
2019-11-10 20:40:38 +0530

    sample

--- FoodiesApp/src/main/java/com/spring/model/Menu.java
+++ FoodiesApp/src/main/java/com/spring/model/Menu.java
@@ -21,7 +21,7 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 public class Menu implements Serializable{
 	
 	/**
-	 * 
+	 * 121212
 	 */
 	private static final long serialVersionUID = 1L;
 
