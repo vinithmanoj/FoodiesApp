dd83a029f80f0c7b04a18f5f3e030cac3d819a57
vinith <vinithmanoj@gmail.com>
2019-11-10 20:04:41 +0530

    Remove unused imports

--- FoodiesApp/src/main/java/com/spring/model/Menu.java
+++ FoodiesApp/src/main/java/com/spring/model/Menu.java
@@ -2,7 +2,6 @@ package com.spring.model;
 
 import java.io.Serializable;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
